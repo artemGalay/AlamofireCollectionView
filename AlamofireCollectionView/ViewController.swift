6a05c8909fe561f9977b29167e24027f79c010ed
Artem Galay <iostema@mail.ru>
2023-01-29 22:09:42 +0300

    Initial Commit

---
+++ AlamofireCollectionView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AlamofireCollectionView
+//
+//  Created by Артем Галай on 29.01.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
